efa9a5eb2690676102f91abcadbb55d512894600
elene <elene.andguladze33b@geolab.edu.ge>
2023-06-23 01:38:56 +0400

    update pagination

--- main.js
+++ main.js
@@ -17,10 +17,20 @@ const closeNav = () => {
 closeNavBtn.addEventListener('click', closeNav);
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    
     },
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+
+        1024: {
+            slidesPerView: 3
+        }
+    }
   });
