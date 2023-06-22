1bfda60bde3198b5a3d68e2808e9f90c35489029
elene <elene.andguladze33b@geolab.edu.ge>
2023-06-23 01:21:02 +0400

    add swiper js

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ const closeNav = () => {
 }
 
 closeNavBtn.addEventListener('click', closeNav);
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
